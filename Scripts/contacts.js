3a11d7ae031b8ba4ad3662ad10898c80ff5c4570
taran-mega <taranjit260526@gmail.com>
2026-09-16 18:17:20 +0530

    Add files via upload

--- Scripts/contacts.js
+++ Scripts/contacts.js
@@ -1,4 +1,23 @@
 // Get Data from HTML
+const contactsList = document.getElementById("contacts-list");
+const emptyState = document.getElementById("empty-state");
+
+// Function for adding Contact on Screen
+function addContact(username){
+    
+    // Create Div
+    const div = document.createElement("div");
+    div.className = "contact";
+    
+    // Add Content
+    div.textContent = username;
+    
+    // Add To Screen
+    contactsList.appendChild(div);
+    
+    // Hide Empty State
+    emptyState.style.display = "none";
+}
 
 // Function for sending request to backend
 async function sendToBackend(){
@@ -7,23 +26,35 @@ async function sendToBackend(){
     API_URL = "https://sky-chat-backend-bl9g.onrender.com";
     
     // Make Controller
-    const controller= new AbortController();
+    const controller = new AbortController();
     
     // Fetch URL
    const response  = await fetch(
-        `${API_URL}/contacts`,
+        `${API_URL}/get-contacts`,
         {
             method: "GET",
             credentials: "include",
-            header:{
-                "Content-Type": "application/json"
-            },
             signal: controller.signal
         }
     );
     
     // Convert Response into JSON
     const data = await response.json();
-    
-    console.log(JSON.stringify(data, null, 2));
 }
+
+// Function for Managing Request
+async function manageRequest(){
+    
+    // Send Request to Backend
+    const response = await sendToBackend();
+    
+    // Loop Over the Response
+    for (let item of response){
+        
+        // Add To Contact
+        addContact(item.username);
+    }
+}
+
+// Initial function
+manageRequest();
